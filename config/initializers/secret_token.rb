2259e202689eba4d1a7bd80ffd2175cc54e0f316
Fernando <feromes@gmail.com>
2011-11-29 15:14:42 -0200

    My first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuerodarMe::Application.config.secret_token = '47f1f72cc344d818d2d072a6c8e6d87d28f02302a42a3b6eec903edd4e9bdaa563d706344ff4b7dcdd597d0d37ac7f01fc0bf0b5e687785bcbe8a92ec78da2a4'
