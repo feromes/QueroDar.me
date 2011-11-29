becdbf33edb3945d239fc8c95630ff3e2a9ed957
Fernando <feromes@gmail.com>
2011-11-29 17:58:40 -0200

    Testando o Seed de Categoria

--- spec/models/categoria_spec.rb
+++ spec/models/categoria_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
+load "#{Rails.root}/db/seeds.rb"
 
 describe Categoria do
   it { should validate_presence_of(:nome) }
+  it "should be seeded" do 
+    categorias = Categoria.find(:all)
+    categorias.count.should be > 3
+  end
+  it "should respect the order of creation" do
+    categorias = Categoria.find([1,2,3])
+    puts categorias.map {|c| c.nome }
+    categorias.map {|c| c.nome }.should be == ["Livros", "Roupas", "Brinquedos"]
+  end
 end
